bf6c7003cd728eb2bda9db3952ac3b8928cf45c5
Divyansh Chauhan <divyanshchauhan28@gmail.com>
2025-10-27 01:52:20 +0530

    Implement Identity framework with database context and initial migration

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
+using Authentication.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+IServiceCollection serviceCollection = builder.Services;
+ServiceLifetime contextLifetime = ServiceLifetime.Scoped;
+ServiceLifetime optionsLifetime = ServiceLifetime.Scoped;
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    
+Action<DbContextOptionsBuilder> optionsAction = (DbContextOptionsBuilder options) =>
+{
+    options.UseSqlServer(connectionString);
+};
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<AuthDbContext>(optionsAction, contextLifetime, optionsLifetime);
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AuthDbContext>();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
